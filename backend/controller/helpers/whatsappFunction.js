fc64b5818dfbdaa755393401ea05b8382539c407
Mudit Nema <78261286+MuditNema@users.noreply.github.com>
2023-03-27 07:43:15 +0530

    Update whatsappFunction.js

--- backend/controller/helpers/whatsappFunction.js
+++ backend/controller/helpers/whatsappFunction.js
@@ -1,14 +1,14 @@
 var axios = require('axios');
 var qs = require('qs');
 var data = qs.stringify({
-    "token": "zhpiqwld5v9c2lev",
+    "token": "",
     "to": "+919978900829",
     "body": "This is a general reminder for you to continue your scholarship procedure"
 });
 
 var config = {
   method: 'post',
-  url: 'https://api.ultramsg.com/instance41408/messages/chat',
+  url: '',
   headers: {  
     'Content-Type': 'application/x-www-form-urlencoded'
   },
